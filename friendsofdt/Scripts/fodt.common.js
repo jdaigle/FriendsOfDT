488301a0b2c0731bf2ffc569bafcffb02cba1642
jdaigle <joseph@cridion.com>
2011-11-28 23:27:47 -0500

    testing new thingy

--- friendsofdt/Scripts/fodt.common.js
+++ friendsofdt/Scripts/fodt.common.js
@@ -1,1 +1,11 @@
 ﻿if (typeof (fodt) == "undefined") fodt = {};
+
+$(function () {
+    "placeholder" in document.createElement("input") || $("textarea[placeholder], input[placeholder]").addClass("gray").each(function () {
+        var a = $(this), b = a.attr("placeholder");
+        a.val() == "" && a.val(b)
+    });
+    $("textarea.gray, input.gray").focus(function () {
+        $(this).attr("rows", 7).filter(".gray").removeClass("gray").val("")
+    });
+});
