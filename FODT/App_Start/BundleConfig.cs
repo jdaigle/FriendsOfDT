74e898a2392cd342fbf8c0e452835e43b232e572
Joseph Daigle <joseph@cridion.com>
2013-06-05 15:39:27 -0400

    spike which utilizes a brand new design - wee

--- FODT/App_Start/BundleConfig.cs
+++ FODT/App_Start/BundleConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Optimization;
+using dotless.Core;
 
 namespace FODT
 {
@@ -7,53 +8,25 @@ namespace FODT
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //// Stylesheets
-            //bundles.Add(new StyleBundle("~/Assets/css/site").Include(
-            //            "~/assets/css/bootstrap.css",
-            //            "~/assets/css/bootstrap.mods.css",
-            //            "~/assets/css/bootstrap.tablesorter.css",
-            //            "~/assets/css/cw.common.css",
-            //            "~/assets/css/cw.tables.css",
-            //            "~/assets/css/cw.table-expandable.css",
-            //            "~/assets/css/cw.icons.css"
-            //            ));
-            //bundles.Add(new StyleBundle("~/Assets/markdown/css").Include(
-            //            "~/Assets/markdown/Markdown.Editor.css"
-            //            ));
-            //bundles.Add(new StyleBundle("~/ControlPanel/Assets/css/area").Include(
-            //            "~/Areas/ControlPanel/assets/css/styles.css"
-            //            ));
+            // Stylesheets
+            var css = new StyleBundle("~/Assets/css/site").Include(
+                        "~/assets/css/bootstrap.css",
+                        "~/assets/css/fodt-main.css",
+                        "~/assets/css/fodt-forms.css",
+                        "~/assets/css/fodt-tables.css",
+                        "~/assets/css/fodt-slideshow.css"
+                        );
+            css.Transforms.Add(new LessTransform());
+            bundles.Add(css);
+        }
 
-            //// Scripts
-            //bundles.Add(new ScriptBundle("~/Assets/js/site").Include(
-            //            "~/Assets/js/jquery.js",
-            //            "~/Assets/js/jquery.cookie.js",
-            //            "~/Assets/js/jquery.color.js",
-            //            "~/Assets/js/jquery.maskedinput-1.3.js",
-            //            "~/Assets/js/jquery.tablesorter.js",
-            //            "~/Assets/js/jsrender.js",
-            //            "~/Assets/js/jquery.observable.js",
-            //            "~/Assets/js/jquery.views.js",
-            //            "~/Assets/js/json2.js",
-            //            "~/Assets/js/date.format.js",
-            //            "~/Assets/js/bootstrap.js",
-            //            "~/Assets/js/bootstrap-sortablelist.js",
-            //            "~/Assets/js/nfvalidate.js",
-            //            "~/Assets/js/code/cw.common.js",
-            //            "~/Assets/js/code/cw.mvc.js",
-            //            "~/Assets/js/code/cw.forms.js",
-            //            "~/Assets/js/code/cw.validators.js",
-            //            "~/Assets/js/code/cw.table-expandable.js",
-            //            "~/Assets/js/code/cw.changepassword.js",
-            //            "~/Assets/js/code/cw.tabs.js",
-            //            "~/Assets/js/code/plugins/cw.tableCheckAll.js",
-            //            "~/Assets/js/code/plugins/cw.selectList.js"
-            //            ));
-            //bundles.Add(new ScriptBundle("~/Assets/markdown/js").Include(
-            //            "~/Assets/markdown/Markdown.Converter.js",
-            //            "~/Assets/markdown/Markdown.Sanitizer.js",
-            //            "~/Assets/markdown/Markdown.Editor.js"
-            //            ));
+        public class LessTransform : IBundleTransform
+        {
+            public void Process(BundleContext context, BundleResponse response)
+            {
+                response.Content = Less.Parse(response.Content);
+                response.ContentType = "text/css";
+            }
         }
     }
 }
