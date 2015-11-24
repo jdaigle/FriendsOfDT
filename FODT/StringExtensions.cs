62be45007de0997b755a809b72a3fe35bafd914d
Joseph Daigle <joseph.daigle@intellinet.com>
2015-11-24 10:37:59 -0500

    helper func

---
+++ FODT/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace FODT
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrWhiteSpace(this string value)
+        {
+            return string.IsNullOrWhiteSpace(value);
+        }
+    }
+}
