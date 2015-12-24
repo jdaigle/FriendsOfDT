d2dd7fc8b3804e47a8ee26531a8fcd759143b960
Joseph Daigle <joseph.daigle@gmail.com>
2015-12-24 17:21:12 -0500

    admin tabs

---
+++ src/FODT/Views/Admin/AdminTabsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using FODT.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FODT.Views.Admin
+{
+    public class AdminTabsViewModel
+    {
+        public AdminTabsViewModel(UrlHelper url)
+        {
+            UserAdminListURL = url.GetURL<UserAdminController>(c => c.List());
+        }
+
+        public string UserAdminListURL { get; set; }
+    }
+}
