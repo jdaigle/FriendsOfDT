5d8e613c63ddd075acf07fba37a970f06c1e7f1d
Joseph Daigle <joseph.daigle@gmail.com>
2016-04-01 14:27:35 -0400

    render as child action

---
+++ src/FODT/ChildActionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using System.Web.Routing;
+using ExpressionHelper = Microsoft.Web.Mvc.Internal.ExpressionHelper;
+
+namespace FODT
+{
+    public static class ChildActionExtensions
+    {
+        public static void RenderAction<TController>(this HtmlHelper htmlHelper, Expression<Action<TController>> action) where TController : Controller
+        {
+            RouteValueDictionary routeValues = ExpressionHelper.GetRouteValuesFromExpression(action);
+            System.Web.Mvc.Html.ChildActionExtensions.RenderAction(htmlHelper, routeValues["action"] as string, routeValues);
+        }
+    }
+}
---
+++ src/FODT/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using FODT.Views.Admin;
+
+namespace FODT.Controllers
+{
+    public class AdminController : BaseController
+    {
+        [Route("admin/components/tabs")]
+        [ChildActionOnly]
+        public ActionResult GetAdminTabs()
+        {
+            return PartialView(new AdminTabsViewModel(Url));
+        }
+    }
+}
